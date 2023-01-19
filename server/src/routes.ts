dad8a87b1b99bdb0fcd7a75e2a3b76a7dd04861b
Vitor Marques <civitao011@gmail.com>
2023-01-19 11:22:50 -0300

    finished backend routes and made front-end first part

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -31,4 +31,45 @@ app.post('/habits', async (request) => {
    return ('')
  })
  
+ app.get('/day', async (request) => {
+  const getDayParams = z.object({
+    date: z.coerce.date()
+  })
+
+  const { date } = getDayParams.parse(request.query)
+
+  const parsedDate = dayjs(date).startOf('day')
+  const weekDay = parsedDate.get('day')
+  
+  
+  const possibleHabits = await prisma.habit.findMany({
+    where: {
+      created_at: {
+        lte: date,
+      },
+      weekDays: {
+      some: {
+        week_day: weekDay
+      }  
+      }
+    }
+  })
+  const day = await prisma.day.findUnique({
+    where: {
+      date: parsedDate.toDate()
+    },
+    include: {
+     dayHabits: true 
+    }
+  })
+
+  const completedHabits = day?.dayHabits.map(dayHabit => {
+    return dayHabit.habit_id
+  })
+ 
+  return { 
+    possibleHabits,
+    completedHabits
+  }
+ })
 }            
---
+++ web/src/components/SummaryTabke.tsx
@@ -0,0 +1,45 @@
+import { generateDaysFromYearBeggining } from "../utils/generate-days-from-year-beggining"
+import { HabitDay } from "./HabitDay"
+
+const weekDays = ['D', 'S', 'T', 'Q', 'Q', 'S', 'S']
+
+const summaryDates = generateDaysFromYearBeggining()
+
+const minimumSummaryDatesSize = 18 * 7 // 18 weeks 
+const ammountOfDaysToFill =  minimumSummaryDatesSize - summaryDates.length
+
+export function SummaryTable() {
+  return(
+    <div className="w-full flex">
+      <div className="grid grid-rows-7 grid-flow-row">
+        {weekDays.map((weekDay, i ) => {
+          return (
+            <div 
+              key={`${weekDay}-${i}`} 
+              className="text-zinc-400 text-xl font-bold h-10 w-10 flex items-center justify-center"
+            >
+              { weekDay}
+            </div>
+          )
+        })}
+      </div>
+      <div className="grid grid-rows-7 grid-flow-col gap-3">
+
+        {summaryDates.map(date => {
+          return (
+           <HabitDay key={date.toString()} />
+           )
+        })}
+          {ammountOfDaysToFill > 0 && Array.from({ length: ammountOfDaysToFill }).map((_, i) => {
+            return ( 
+              <div 
+                key={i} 
+                className="w-10 h-10 bg-zinc-900 border-zinc-800 border-2 rounded-lg opacity-40 cursor-not-allowed"></div>
+
+            )
+          })}
+
+      </div>
+    </div>
+  )
+}
